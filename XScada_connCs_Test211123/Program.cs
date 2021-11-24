d8cb16d053923d8a4d40c35f7040f6b57b6b01d7
SW_Lee31 <sangwon3232@naver.com>
2021-11-24 09:03:20 +0900

    Add additinal files

---
+++ XScada_connCs_Test211123/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xisom.Scada.Model;
+using Xisom.Scada.Core;
+using Xisom.Scada.Browser;
+using Xisom.Scada.WinForm;
+
+namespace XScada_connCs_Test211123
+{
+    static class Program
+    {
+        public static DocumentContext documentContext = null;
+        public static ID
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
